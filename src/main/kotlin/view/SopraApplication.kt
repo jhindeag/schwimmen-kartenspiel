5f54ed5dad2f80ad65a56500876723c38b41249b
Duc Anh Le <duc-anh.le@tu-dortmund.de>
2021-11-28 19:42:16 +0100

    saving before testing LinearLayout

--- src/main/kotlin/view/SopraApplication.kt
+++ src/main/kotlin/view/SopraApplication.kt
@@ -1,5 +1,6 @@
 package view
 
+import entity.Player
 import service.RootService
 import tools.aqua.bgw.core.BoardGameApplication
 
@@ -10,13 +11,17 @@ class SopraApplication : BoardGameApplication("Schwimmen"), Refreshable {
     private val scoreboardScene = ScoreboardScene(rootService).apply {
         newGame.onMouseClicked = { showMenuScene(mainMenuScene) }
     }
+    private val endTurnScene = EndTurnScene(rootService).apply {
+        confirm.onMouseClicked = { hideMenuScene(500) }
+    }
 
     init {
         rootService.addRefreshables(
             this,
             mainMenuScene,
             inGameScene,
-            scoreboardScene
+            scoreboardScene,
+            endTurnScene
         )
         this.showGameScene(inGameScene)
         this.showMenuScene(mainMenuScene, 500)
@@ -27,7 +32,14 @@ class SopraApplication : BoardGameApplication("Schwimmen"), Refreshable {
     }
 
     override fun refreshAfterEndGame() {
+        this.hideMenuScene()
         this.showMenuScene(scoreboardScene, 500)
+    }
+
+    override fun refreshAfterPlayerStateChange(player: Player) {
+        if (rootService.currentGame != null) {
+            this.showMenuScene(endTurnScene)
+        }
     }
 }
 
